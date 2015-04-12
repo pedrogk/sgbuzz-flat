34ff57f4c779e7b626137e89b7cabf1d490975da
root <root@sg.com.mx>
2015-04-12 18:50:16 -0500

    Updated styles and sesion-conferencia template

---
+++ tpl/sesion-conferencia-node-form.tpl.php
@@ -0,0 +1,21 @@
+<div class="node-add-wrapper clear-block">
+  <div class="node-column-sidebar">
+    <?php if($sidebar): ?>
+      <?php print render($sidebar); ?>
+    <?php endif; ?>
+  </div>
+<h1 class="page-title">Enviar propuesta de sesión para evento</h1>
+  <div class="node-column-main">
+    <?php if($form): ?>
+      <?php print drupal_render_children($form); ?>
+    <?php endif; ?>
+  
+    <?php if($buttons): ?>
+      <div class="node-buttons">
+        <?php print render($buttons); ?>
+    </div>
+  <?php endif; ?>
+  </div>
+  <div class="clear"></div>
+</div>
+
